2df66b8ede7d34c27bded0218ccde842e69ae04b
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2025-01-28 17:11:32 -0800

    Add method  to process client request

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -5,8 +5,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TCPServer {
-    public static void main(String[] args) throws IOException {
+    private KVStore store = new KVStore();
 
+    public void start(String[] args) throws IOException {
         // Validate that user provided a port number in the command line arguments
         if (args.length < 1) {
             System.out.println("Usage: java TCPServer <port number>");
@@ -26,6 +27,7 @@ public class TCPServer {
             return;
         }
 
+        // Initialize server socket and connection socket to accept client request
         ServerSocket serverSocket = null;
         Socket connectionSocket = null;
 
@@ -33,17 +35,23 @@ public class TCPServer {
             // Set up the server socket
             serverSocket = new ServerSocket(port);
             log("Server listening on port: " + port);
-            // Listen for client request
+            // Listen for client requests until manually stopped
             while (true) {
                 connectionSocket = serverSocket.accept();
                 log("Client connected: " + connectionSocket.getInetAddress());
+                // Set up input and output streams
                 DataInputStream in = new DataInputStream(connectionSocket.getInputStream());
                 DataOutputStream out = new DataOutputStream(connectionSocket.getOutputStream());
                 // Get input from client
                 String data = in.readUTF();
-                log("Received query from client at" + connectionSocket.getInetAddress() + ":" + port + " " + data);
-                // Prepare response to client
-                out.writeUTF("Message from server: " + data);
+                log("Received request of length " + data.length() + "from " + connectionSocket.getInetAddress() + ":" + connectionSocket.getPort() + "\n" + data);
+                // Process client request and send it back
+                try {
+                    String output = processRequest(data);
+                    out.writeUTF(output);
+                } catch (IllegalArgumentException e) {
+                    log("Received malformed request of length " + data.length() + "from " + connectionSocket.getInetAddress() + ":" + connectionSocket.getPort() + "\n Message: " + e.getMessage());
+                }
             }
         } catch (IOException e) {
             log("Error while listening on port " + port + ": " + e.getMessage());
@@ -55,14 +63,65 @@ public class TCPServer {
                 connectionSocket.close();
             }
         }
-    }
 
+    }
     /**
      * Helper method to print logs in the terminal with current system timestamp to millisecond precision
+     *
      * @param message message to be printed
      */
-    private static void log(String message) {
+    private void log(String message) {
         System.out.println("[Server]" + System.currentTimeMillis() + " " + message);
     }
 
+    /**
+     * Helper method to process the client request. A valid request should be in the format of PUT key value | GET key | DELETE key'
+     *
+     * @param request a string of client request
+     * @return a string message of the status whether the operation is successful
+     * @throws IllegalArgumentException if the request does not comply with the pre-defined format.
+     */
+    private String processRequest(String request) throws IllegalArgumentException {
+        String[] tokens = request.split(" ");
+        // Validate request conforms with defined protocol
+        if (tokens.length < 2) {
+            throw new IllegalArgumentException("Invalid request format. Usage: PUT key value | GET key | DELETE key");
+        }
+        String command = tokens[0].toUpperCase();
+        switch (command) {
+            case "PUT":
+                if (tokens.length == 3) {
+                    store.put(tokens[1], tokens[2]);
+                    return "Ok. Added " + tokens[1] + ":" + tokens[2] + " to the store";
+                }
+                throw new IllegalArgumentException("Invalid request format. Usage: PUT key value");
+            case "GET":
+                if (tokens.length == 2) {
+                    String value = store.get(tokens[1]);
+                    if (value != null) {
+                        return "Ok. Found " + tokens[1] + ":" + value;
+                    }
+                    throw new IllegalArgumentException("key does not exist");
+                }
+                throw new IllegalArgumentException("Invalid request format. Usage: GET key");
+            case "DELETE":
+                if (tokens.length == 2) {
+                    String value = store.get(tokens[1]);
+                    if (value != null) {
+                        store.delete(tokens[1]);
+                        return "Ok. Deleted " + tokens[1] + ":" + value;
+                    }
+                    throw new IllegalArgumentException("key does not exist");
+                }
+                throw new IllegalArgumentException("Invalid request format. Usage: DELETE key");
+            default:
+                throw new IllegalArgumentException("Invalid command. Usage: PUT key value | GET key | DELETE key");
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        TCPServer server = new TCPServer();
+        server.start(args);
+    }
 }
