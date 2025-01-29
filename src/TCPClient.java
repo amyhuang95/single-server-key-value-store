851f9d37b2c4d25e4a3700c81f4aeea2e13efdc5
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2025-01-28 17:15:23 -0800

    Refactor client functionality to start method

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -6,28 +6,8 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class TCPClient {
-    public static void main(String[] args) {
 
-        // Validate that user provided a hostname/ip address and a port number in the command line arguments
-        if (args.length < 2) {
-            System.out.println("Usage:java TCPClient <host name/IP address> <port number>");
-            System.exit(1);
-        }
-        String host = args[0];
-        int port;
-
-        // Validate input port number
-        try {
-            port = Integer.parseInt(args[1]);
-            if (port < 1024 || port > 65535) {
-                throw new IllegalArgumentException("Port number must be between 1024 - 65535.");
-            }
-        } catch (Exception e) {
-            System.out.println("Invalid port number: " + e.getMessage());
-            System.exit(1);
-            return;
-        }
-
+    public void start(String host, int port) {
         // Set up client
         try (Socket socket = new Socket(host, port)) {
             System.out.println("[Client]" + System.currentTimeMillis() + " Connected to host: " + host + " port number: " + port);
@@ -53,4 +33,28 @@ public class TCPClient {
         System.out.println("[Client]" + System.currentTimeMillis() + " " + message);
     }
 
+    public static void main(String[] args) {
+        // Validate that user provided a hostname/ip address and a port number in the command line arguments
+        if (args.length < 2) {
+            System.out.println("Usage:java TCPClient <host name/IP address> <port number>");
+            System.exit(1);
+        }
+        String host = args[0];
+        int port;
+
+        // Validate input port number
+        try {
+            port = Integer.parseInt(args[1]);
+            if (port < 1024 || port > 65535) {
+                throw new IllegalArgumentException("Port number must be between 1024 - 65535.");
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid port number: " + e.getMessage());
+            System.exit(1);
+            return;
+        }
+
+        TCPClient client = new TCPClient();
+        client.start(host, port);
+    }
 }
